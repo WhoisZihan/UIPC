df0d5e6c0d3dded22ae94ff87e5026ba6dbb9cc3
Zihan Yang <whois.zihan.yang@gmail.com>
2018-08-01 16:37:20 +0800

    Replace argp library with getopt
    argp library is too heavyweight, and does not have features I need

--- uipc_user.c
+++ uipc_user.c
@@ -9,71 +9,33 @@
 static char doc[] =
   "UIPC - user-level helper function";
 
-/* A description of the arguments we accept. */
-static char args_doc[] = "ENTER|TRIGGER";
-
-/* The options we understand. */
-static struct argp_option options[] = {
-  {"enter", 'e', 0, 0, "enter mwait state" },
-  {"trigger", 't', 0, 0, "write to monitored memory to trigger mwait" },
-  { 0 }
-};
-
-/* Used by main to communicate with parse_opt. */
-struct arguments
-{
-  char *args[1];                /* arg1 & arg2 */
-  int choice;
-};
-
-/* Parse a single option. */
-static error_t
-parse_opt (int key, char *arg, struct argp_state *state)
-{
-  /* Get the input argument from argp_parse, which we
-     know is a pointer to our arguments structure. */
-  struct arguments *arguments = state->input;
-
-  switch (key)
-    {
-    case 'e':
-      arguments->choice = UIPC_ENTER_MONITOR_MWAIT;
-      break;
-    case 't':
-      arguments->choice = UIPC_TRIGGER_MONITOR;
-      break;
-    case ARGP_KEY_ARG:
-      if (state->arg_num >= 1)
-        /* Too many arguments. */
-        argp_usage (state);
-
-      arguments->args[state->arg_num] = arg;
-
-      break;
-
-    case ARGP_KEY_END:
-      if (state->arg_num < 1)
-        /* Not enough arguments. */
-        argp_usage (state);
-      break;
-
-    default:
-      return ARGP_ERR_UNKNOWN;
-    }
-  return 0;
-}
-
 int main(int argc, char *argv[])
 {
-    //if (argc != 2) {
-    //    printf("Usage:\n./program ENTER|TRIGGER\n");
-    //    return 0;
-    //}
+    if (argc != 2) {
+        printf("Usage:\n./program ENTER|TRIGGER\n");
+        return 0;
+    }
 
-    /* parse arguments using argp library */
-    struct arguments arguments;
-    struct argp argp = { options, parse_opt, args_doc, doc };
-    argp_parse (&argp, argc, argv, 0, 0, &arguments);
+    char ch;
+    int choice = -1;
+
+    while ((ch = getopt(argc, argv, "eth")) != -1) {
+        switch (ch) {
+        case 'e':
+            choice = UIPC_ENTER_MONITOR_MWAIT;
+            break;
+        case 't':
+            choice = UIPC_TRIGGER_MONITOR;
+            break;
+        default:
+            choice = -1;
+            break;
+        }
+    }
+    if (choice < 0) {
+        printf("Usage: ./uipc -e|-t\n\t-e enter mwait state\n\t-t trigger monitored memory");
+        return 0;
+    }
 
     int fd = open("/dev/uipc-mwait", O_RDWR);
     if (fd < 0) {
